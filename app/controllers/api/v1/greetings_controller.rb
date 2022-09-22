76f7f049ad61b695562ed77829a157f55f5f0a8c
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-09-22 18:03:01 +0100

    Add features

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::GreetingsController < ApplicationController
   # This is a before action that is called before the show, update, and destroy actions. It is setting the @greeting
   # variable to the greeting that is being requested.
-  before_action :set_greeting, only: %i[show update destroy]
+  before_action :set_greeting, only: %i[show]
 
   # The index function is a GET request that returns all the greetings in the database
   def index
@@ -12,30 +12,6 @@ class Api::V1::GreetingsController < ApplicationController
   # The show function is a GET request that returns a single greeting from the database
   def show
     render json: @greeting
-  end
-
-  # The create function is a POST request that creates a new greeting in the database
-  def create
-    @greeting = Greeting.new(greeting_params)
-    if @greeting.save
-      render json: @greeting, status: :created, location: @greeting
-    else
-      render json: @greeting.errors, status: :unprocessable_entity
-    end
-  end
-
-  # The update function is a PUT request that updates a greeting in the database
-  def update
-    if @greeting.update(greeting_params)
-      render json: @greeting
-    else
-      render json: @greeting.errors, status: :unprocessable_entity
-    end
-  end
-
-  # The destroy function is a DELETE request that deletes a greeting from the database
-  def destroy
-    @greeting.destroy
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,6 @@ Rails.application.routes.draw do
     namespace :v1 do
       get 'messages', to: 'greetings#index'
       get 'messages/:id', to: 'greetings#show'
-      post 'messages', to: 'greetings#create'
-      put 'messages/:id', to: 'greetings#update'
-      delete 'messages/:id', to: 'greetings#destroy'
     end
   end
 end
