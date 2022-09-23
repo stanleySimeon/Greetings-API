4a21e20ad2ab003270d6e130482fabe89656dde4
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-09-23 02:41:38 +0100

    add features

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'greetings#hello'
+  root 'welcome#index'
   namespace :api, defaults: { format: :json } do
     namespace :v1, defaults: { format: :json } do
       get 'messages', to: 'greetings#index'
